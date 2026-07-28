7e891a1c11758969987d48dbc7cd7bcc895641cc
rodrigosyonet-commits <rodrigo.syonet@gmail.com>
2026-07-28 10:58:09 -0600

    Update state.ts

--- lib/state.ts
+++ lib/state.ts
@@ -51,3 +51,34 @@ export function deleteSession(
 ) {
   sessions.delete(chatId);
 }
+
+export type UserState = {
+  step: string;
+
+  name?: string;
+  email?: string;
+  description?: string;
+
+  telegramId?: string;
+  username?: string;
+
+  latitude?: number;
+  longitude?: number;
+};
+
+const states = new Map<number, UserState>();
+
+export function getUserState(chatId: number) {
+  return states.get(chatId);
+}
+
+export function setUserState(
+  chatId: number,
+  data: UserState
+) {
+  states.set(chatId, data);
+}
+
+export function clearUserState(chatId: number) {
+  states.delete(chatId);
+}
